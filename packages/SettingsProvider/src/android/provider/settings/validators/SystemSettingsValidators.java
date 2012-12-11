394ff8948bc400e83e8fbe917f1720068cef3bfa
Roman Birg <romanbirg@gmail.com>
2012-12-11 04:12:41 -0600

    SystemUI: add left and right virtual buttons while typing
    [cyanogen] refactor for cm-14.0
    [LuK1337] update for 8.1
    [sam3000] Updates for ten
    
    * Removed keyRepeat attribute from layouts because it defaults to true
      and AOSP removed it anyway in commit 160ad6b
    
    * Removed layout-sw600dp layout changes because AOSP refactored to use
      layout instead as seen in commit aa94d95b
    
    * Renamed rot90 layout suffix to vertical as per AOSP changes
    
    [maniac103] More refactorings for ten
    
    * Follow new system of inflating all views dynamically
    
    Squashes the following commits:
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Wed Aug 6 09:16:35 2014 +0200
    
        Fix side button visibility tracking.
    
        Previously one could e.g. get legacy menu button being visible in
        certain scenarios, like this one:
        - Open legacy app, e.g. CatLog
        - Invoke IME
        - Lock screen
        - Unlock screen
        - Result: Menu button visible in keyguard
    
        Change-Id: Ia796f73ec995ea1cc34d38f8aefdbccda7be7565
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Wed Jul 2 09:31:23 2014 -0700
    
        SystemUI: fix up navigation cursor keyevent flags
    
        Volume cursor controls use slightly different flags to keep the cursor
        within the field. Match them in the SystemUI for consistent (and better)
        behavior.
    
        Change-Id: I56d597f111ec611b0ca24a10f30575401fa76d18
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Jul 3 12:04:40 2014 -0700
    
        SystemUI: fix up initial cursor down flags on nav bar
    
        We were using the regular flags for the initial ACTION_DOWN event so the
        FLAG_KEEP_TOUCH_MODE was not being passed and allowed the cursor to jump
        out of bounds slightly.
    
        Change-Id: I5b4d2a5e5e0ce562985fcc3b7b6fac8b4abc49da
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu Apr 19 01:14:21 2018 +0200
    
        SystemUI: Fix navigation bar arrows visibility handling
    
        * Fixes issue where input events would be sent
          while dpad_group view was set to be invisible.
    
        Change-Id: Ie802e9c8157505a2ad706332f0b2fe715caf428c
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sun Apr 19 23:17:12 2020 +0100
    
        NavigationBarView: Fix cursor keys visibility with gestural mode enabled
    
        When gestural mode is enabled and the IME is being shown
        the cursor keys must be hidden, otherwise there's an overlap.
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/1730
        Change-Id: I2f34810d7b137573360a140bbf267028363e6a28
    
    Change-Id: Iea446443d33ff305765e47c86523b619676116ac

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -256,5 +256,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.NOTIFICATION_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 4));
         VALIDATORS.put(System.NAV_BAR_COMPACT_LAYOUT, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.NAVIGATION_BAR_HINT, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.NAVIGATION_BAR_MENU_ARROW_KEYS, BOOLEAN_VALIDATOR);
     }
 }
