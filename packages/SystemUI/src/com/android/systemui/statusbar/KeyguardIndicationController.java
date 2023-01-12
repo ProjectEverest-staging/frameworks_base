5420ccb23d0fb8e321235344d2f796199da1d934
ShevT <evgeny.shishkov@gmail.com>
2023-01-12 14:24:12 +0300

    Add config overlay for battery info update
     It is not necessary to use this for all devices, but only for those
     that have a problem with charging display (for example OnePlus 7 series)
    
    Change-Id: I98441087ec450ec3a2699a1a79b84bc32a1fe340
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -225,6 +225,7 @@ public class KeyguardIndicationController {
     private String mMessageToShowOnScreenOn;
 
     private IBatteryPropertiesRegistrar mBatteryPropertiesRegistrar;
+    private boolean mAlternateFastchargeInfoUpdate;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
@@ -382,9 +383,13 @@ public class KeyguardIndicationController {
 
         mStatusBarStateListener.onDozingChanged(mStatusBarStateController.isDozing());
 
-        mBatteryPropertiesRegistrar =
+        mAlternateFastchargeInfoUpdate =
+                    mContext.getResources().getBoolean(R.bool.config_alternateFastchargeInfoUpdate);
+        if (mAlternateFastchargeInfoUpdate) {
+            mBatteryPropertiesRegistrar =
                     IBatteryPropertiesRegistrar.Stub.asInterface(
                     ServiceManager.getService("batteryproperties"));
+        }
     }
 
     public void setIndicationArea(ViewGroup indicationArea) {
@@ -1350,7 +1355,7 @@ public class KeyguardIndicationController {
                 mKeyguardLogger.log(TAG, ERROR, "Error calling IBatteryStats", e);
                 mChargingTimeRemaining = -1;
             }
-            if (wasPluggedIn != mPowerPluggedIn) {
+            if (mAlternateFastchargeInfoUpdate && (wasPluggedIn != mPowerPluggedIn)) {
                 if (mPowerPluggedIn) {
                     mUpdateInfo.run();
                 } else {
