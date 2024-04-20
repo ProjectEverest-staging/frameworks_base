e1020f767a490e11269870d1a2a7ba2d95b49f36
janak <niraulajanak2019@gmail.com>
2024-04-20 19:08:44 +0100

    Revert "SystemUI: fixup! Add less boring heads up option to 14 QPR2"
    This reverts commit b654e7bb2dbedc340aa171421e67bc2a94f54c3f.

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
@@ -28,7 +28,6 @@ import static com.android.systemui.statusbar.notification.interruption.Notificat
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.content.ContentResolver;
 import android.database.ContentObserver;
 import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.Handler;
@@ -89,7 +88,6 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
     private final SystemClock mSystemClock;
     private final GlobalSettings mGlobalSettings;
     private final EventLog mEventLog;
-    private final ContentResolver mContentResolver;
 
     @VisibleForTesting
     protected boolean mUseHeadsUp = false;
@@ -128,7 +126,6 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
 
     @Inject
     public NotificationInterruptStateProviderImpl(
-	    ContentResolver contentResolver,
             Context context,
             PowerManager powerManager,
             AmbientDisplayConfiguration ambientDisplayConfiguration,
@@ -146,7 +143,6 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
             SystemClock systemClock,
             GlobalSettings globalSettings,
             EventLog eventLog) {
-        mContentResolver = contentResolver;
         mContext = context;
         mTm = (TelecomManager) context.getSystemService(Context.TELECOM_SERVICE);
         mPowerManager = powerManager;
