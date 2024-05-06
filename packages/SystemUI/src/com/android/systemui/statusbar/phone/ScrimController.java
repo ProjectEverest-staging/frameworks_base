8096ac23d041ab78fbad2d5a900ca9edb0bff3db
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-06 18:42:44 +0800

    SystemUI: Introduce Depth Wallpaper feature [1/2]
    * like ios wallpaper depth, taken from pixel xpert
    * todo: implement subject segmentation when we found a way to import all gms and ml kit dependencies
      aside from importing aars (note to other source maintainers: i tried and but the dependencies were too many so i dropped subject segmentation impl)
    
    Co-authored-by: Siavash <siavash@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -607,6 +607,10 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     public ScrimState getState() {
         return mState;
     }
+    
+    public float getScrimBehindAlpha() {
+        return mScrimBehindAlphaKeyguard;
+    }
 
     /**
      * Sets the additional scrim behind alpha keyguard that would be blended with the default scrim
@@ -1096,6 +1100,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                         AlarmTimeout.MODE_IGNORE_IF_SCHEDULED);
             });
         }
+        com.android.systemui.util.WallpaperDepthUtils.getInstance(mScrimBehind.getContext()).updateDepthWallpaper();
     }
 
     /**
